f5b6e7ad5be593df68b0a1e662aa60e5a0c3cd1e
alexey <alexey.pub@gmail.com>
2018-04-07 23:17:18 +0300

    Request and Response

--- classes/Responses/HttpResponse.php
+++ classes/Responses/HttpResponse.php
@@ -9,7 +9,30 @@
 namespace SiteFramework\Responses;
 
 
-abstract class HttpResponse
+class HttpResponse
 {
+    const CONTENT_TYPE_TEXTHTML="text/html";
+    const CONTENT_TYPE_JSON="application/json";
+    const ENCODING_UTF8="utf8";
 
+    private $_contentType=self::CONTENT_TYPE_TEXTHTML;
+    private $_contentEncoding=self::ENCODING_UTF8;
+
+
+    function getContentType():string{
+        $ct=$this->_contentType;
+        if($ct==self::CONTENT_TYPE_TEXTHTML){
+            return "Content-Type: ".$ct."; encoding=".$this->_contentEncoding;
+        }else{
+            return "Content-Type: ".$this->_contentType;
+        }
+    }
+
+    function setContentType($contentType):void{
+        $this->_contentType=$contentType;
+    }
+
+    function setEncoding($encoding){
+        $this->_contentEncoding=$encoding;
+    }
 }
